ab6277b5de58960704f999ddb4da7138e2d0c8dd
StefanFlorea00 <stefflr.com>
2020-09-03 20:37:07 +0200

    finished animation, added slider

--- barchart.js
+++ barchart.js
@@ -1,27 +1,86 @@
-let bars = document.querySelectorAll(".bar");
-let customerNumbers;
+"use strict";
 
-console.log(bars);
-calculateBars();
+let queueArray = [];
+let bars = [];
+let loopTimeout;
+const minCustomers = 0,
+  maxCustomers = 32,
+  maxLoopTime = 100,
+  barHeightModifier = 3,
+  loopSpeed = 100,
+  redBarMinHeight = 31;
 
-function getBar(i) {
-  return bars[i];
+let barNumber = 40;
+
+startBarChartAnimation();
+
+let slider = document.getElementById("myRange");
+slider.addEventListener("change", changeBarNumber);
+
+function changeBarNumber() {
+  barNumber = slider.value;
+  console.log(slider.value);
+  document.querySelector(".bars").innerHTML = "";
+  startBarChartAnimation();
+}
+
+function startBarChartAnimation() {
+  initBars();
+  createBars(barNumber);
+  loopBars();
 }
 
 function setBarHeight(i, height) {
-  return (getBar(i).style.height = height + "px");
-}
-
-function calculateBars() {
-  for (let i = 0; i < bars.length; i++) {
-    displayBar(i);
-  }
-}
-
-function displayBar(i) {
-  setBarHeight(i, getCustomerNumber(30, 100));
+  return (bars[i].style.height = height + "px");
 }
 
 function getCustomerNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function initBars() {
+  window.clearTimeout(loopTimeout);
+
+  for (let i = 0; i <= barNumber; i++) {
+    let queueSize = getCustomerNumber(0, maxCustomers);
+    queueArray[i] = queueSize;
+  }
+}
+
+function createBars(nr) {
+  for (let i = 0; i <= nr; i++) {
+    let barElement = document.createElement("div");
+    barElement.className = "bar";
+    document.querySelector(".bars").appendChild(barElement);
+  }
+  bars = document.querySelectorAll(".bar");
+}
+
+function loopBars() {
+  for (let i = 0; i < maxLoopTime; i++) {
+    loopTimeout = setTimeout(() => {
+      queueArray.push(getCustomerNumber(0, 32));
+      queueArray.shift(0);
+      displayBars();
+    }, i * loopSpeed);
+  }
+}
+
+function displayBars() {
+  for (let j = 0; j < bars.length; j++) {
+    displayBar(j, queueArray[j] * barHeightModifier);
+  }
+}
+
+function displayBar(bar, height) {
+  setBarHeight(bar, height);
+  if (bar <= 20) {
+    bars[bar].style.opacity = bar * 4 + "%";
+    bars[bar].style.filter = `grayscale(${(100 - bar * 5) * 2}%)`;
+  }
+  if (parseInt(bars[bar].style.height) / barHeightModifier >= 30) {
+    bars[bar].style.backgroundColor = "red";
+  } else {
+    bars[bar].style.backgroundColor = "green";
+  }
 }
